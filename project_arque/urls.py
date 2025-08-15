457604c7710b3daccefb63e0fa0508bcbd98842c
JoaoDevPsi <psi.joaoaraujo@gmail.com>
2025-08-15 18:05:21 -0300

    pr commit a imagem anterior funcinou
    imagem anterior funcionou, se der erro voltar para antiga

--- project_arque/urls.py
+++ project_arque/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from content_manager.views import ArticleViewSet, GalleryPostViewSet
 from contact_form.views import ContactSubmissionCreateView
 from django.conf import settings
-from django.conf.urls.static import static
-
+from django.views.static import serve
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -24,4 +23,6 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+]
