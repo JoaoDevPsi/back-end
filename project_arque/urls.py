ac51338c41f062c63ac292afbf5a6f43c06d0898
JoaoDevPsi <psi.joaoaraujo@gmail.com>
2026-01-07 12:51:49 -0300

    error404solve

--- project_arque/urls.py
+++ project_arque/urls.py
@@ -2,14 +2,13 @@ from django.contrib import admin
 from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
 from content_manager.views import ArticleViewSet, GalleryPostViewSet
-from contact_form.views import ContactSubmissionCreateView
+from contact_form.views import ContactSubmissionCreateView, api_upload_video
 from django.conf import settings
 from django.views.static import serve
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
-from contact_form.views import api_upload_video
 
 router = DefaultRouter()
 router.register(r'articles', ArticleViewSet)
@@ -19,10 +18,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/contact/', ContactSubmissionCreateView.as_view(), name='contact_submit'),
-
+    path('api/upload-video/', api_upload_video, name='api_upload_video'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/upload-video/', api_upload_video),
 ]
 
 urlpatterns += [
